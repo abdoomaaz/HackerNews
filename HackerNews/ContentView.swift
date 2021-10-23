e8f87a295aaabc685a0f732c28b98fddef70c450
abdoomaaz <abodeh98@gmail.com>
2021-10-23 21:48:13 +0200

    Initial Commit

---
+++ HackerNews/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  HackerNews
+//
+//  Created by playground on 23.10.21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ HackerNews/HackerNewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  HackerNewsApp.swift
+//  HackerNews
+//
+//  Created by playground on 23.10.21.
+//
+
+import SwiftUI
+
+@main
+struct HackerNewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
